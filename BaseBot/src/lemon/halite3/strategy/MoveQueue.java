221c8e74fdd09e764ae9ff000df1e975e7d15d74
awesomelemonade <awesomelemonade@gmail.com>
2018-11-04 11:58:48 -0500

    Prioritized ships that cannot move in MoveQueue

--- BaseBot/src/lemon/halite3/strategy/MoveQueue.java
+++ BaseBot/src/lemon/halite3/strategy/MoveQueue.java
@@ -8,6 +8,7 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 import lemon.halite3.util.Direction;
+import lemon.halite3.util.GameConstants;
 import lemon.halite3.util.GameMap;
 import lemon.halite3.util.Networking;
 import lemon.halite3.util.Ship;
@@ -32,6 +33,14 @@ public class MoveQueue {
 		}
 	}
 	public void resolveCollisions(List<Integer> shipPriorities) {
+		// Handle ships forced to stand still
+		for (int shipId : shipPriorities) {
+			Ship ship = gameMap.getMyPlayer().getShips().get(shipId);
+			if (ship.getHalite() < gameMap.getHalite(ship.getLocation()) / GameConstants.MOVE_COST_RATIO) {
+				unsafe.add(ship.getLocation());
+				map.put(shipId, Direction.STILL);
+			}
+		}
 		// TODO - Handle ship spawning + Creation of dropoffs
 		// TODO - Handle collisions between ships
 		for (int shipId : shipPriorities) {
