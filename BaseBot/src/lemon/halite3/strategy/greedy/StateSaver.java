35f76fe5fef9c1d37ed4b331b016d36cf1a34e6e
awesomelemonade <awesomelemonade@gmail.com>
2018-11-07 21:51:05 -0500

    Added StateSaver

---
+++ BaseBot/src/lemon/halite3/strategy/greedy/StateSaver.java
@@ -0,0 +1,42 @@
+package lemon.halite3.strategy.greedy;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import lemon.halite3.strategy.greedy.GreedyStrategy.MinePlan;
+import lemon.halite3.util.GameMap;
+import lemon.halite3.util.Vector;
+
+public class StateSaver {
+	public static void save(String filename, GameMap gameMap, Map<Vector, Integer> mineMap, Map<Vector, MinePlan> minePlans) {
+		try {
+			PrintWriter writer = new PrintWriter(new FileWriter(filename));
+			writer.println(gameMap.getWidth() + " " + gameMap.getHeight());
+			for (int i = 0; i < gameMap.getWidth(); ++i) {
+				for (int j = 0; j < gameMap.getHeight(); ++j) {
+					writer.print(gameMap.getHalite(i, j) - mineMap.getOrDefault(Vector.getInstance(i, j), 0));
+					writer.print(' ');
+				}
+				writer.println();
+			}
+			for (int i = 0; i < gameMap.getWidth(); ++i) {
+				for (int j = 0; j < gameMap.getHeight(); ++j) {
+					MinePlan plan = minePlans.get(Vector.getInstance(i, j));
+					writer.printf("%d %d %d %d ", plan.getQuad().getLocation().getX(), plan.getQuad().getLocation().getY(),
+							plan.getQuad().getSize().getX(), plan.getQuad().getSize().getY());
+					writer.print(plan.getMineMap().size() + " ");
+					for (Entry<Vector, Integer> entry : plan.getMineMap().entrySet()) {
+						writer.printf("%d %d %d ", entry.getKey().getX(), entry.getKey().getY(), entry.getValue());
+					}
+					writer.println(plan.getCount());
+				}
+			}
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
