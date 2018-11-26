ba37f44efaaa42ba3e9c66021c3409032a5d4e1f
awesomelemonade <awesomelemonade@gmail.com>
2018-11-25 20:21:54 -0500

    Simplified DP

--- BaseBot/src/lemon/halite3/strategy/generator/DP.java
+++ BaseBot/src/lemon/halite3/strategy/generator/DP.java
@@ -9,6 +9,8 @@ import lemon.halite3.util.GameMap;
 import lemon.halite3.util.Vector;
 
 public class DP {
+	private static final int UNCALCULATED = Integer.MAX_VALUE;
+	private static final int QUEUED = Integer.MAX_VALUE - 1;
 	public static int[][][][] execute(GameMap gameMap){
 		int[][][][] costs = new int[gameMap.getWidth()][gameMap.getHeight()][gameMap.getWidth()][gameMap.getHeight()];
 		for (int i = 0; i < gameMap.getWidth(); ++i) {
@@ -21,19 +23,18 @@ public class DP {
 	public static int[][] execute(GameMap gameMap, Vector source) {
 		int[][] dp = new int[gameMap.getWidth()][gameMap.getHeight()];
 		int[][] costs = new int[gameMap.getWidth()][gameMap.getHeight()];
-		boolean[][] queued = new boolean[gameMap.getWidth()][gameMap.getHeight()];
 		for (int i = 0; i < dp.length; ++i) {
 			for (int j = 0; j < dp[0].length; ++j) {
-				dp[i][j] = Integer.MAX_VALUE;
+				dp[i][j] = UNCALCULATED;
 			}
 		}
 		Deque<Vector> queue = new ArrayDeque<Vector>();
-		queued[source.getX()][source.getY()] = true;
+		dp[source.getX()][source.getY()] = QUEUED;
 		costs[source.getX()][source.getY()] = 0;
 		dp[source.getX()][source.getY()] = gameMap.getHalite(source) / GameConstants.MOVE_COST_RATIO;
 		for (Direction direction : Direction.CARDINAL_DIRECTIONS) {
 			Vector v = source.add(direction, gameMap);
-			queued[v.getX()][v.getY()] = true;
+			dp[v.getX()][v.getY()] = QUEUED;
 			queue.add(v);
 		}
 		while (!queue.isEmpty()) {
@@ -41,13 +42,12 @@ public class DP {
 			int min = Integer.MAX_VALUE;
 			for (Direction direction : Direction.CARDINAL_DIRECTIONS) {
 				Vector v = vector.add(direction, gameMap);
-				if (dp[v.getX()][v.getY()] == Integer.MAX_VALUE) {
-					if (!queued[v.getX()][v.getY()]) {
-						queued[v.getX()][v.getY()] = true;
-						queue.add(v);
-					}
-				} else {
-					min = Math.min(min, dp[v.getX()][v.getY()]);
+				int x = dp[v.getX()][v.getY()];
+				if (x == UNCALCULATED) {
+					queue.add(v);
+					dp[v.getX()][v.getY()] = QUEUED;
+				} else if (x != QUEUED) {
+					min = Math.min(min, x);
 				}
 			}
 			costs[vector.getX()][vector.getY()] = min;
