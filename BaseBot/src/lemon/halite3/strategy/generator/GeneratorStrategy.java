839553a179eabdd8e56de28946ba106744cb971a
awesomelemonade <awesomelemonade@gmail.com>
2018-11-27 13:11:26 -0500

    Added shipPriorities to GeneratorStrategy

--- BaseBot/src/lemon/halite3/strategy/generator/GeneratorStrategy.java
+++ BaseBot/src/lemon/halite3/strategy/generator/GeneratorStrategy.java
@@ -1,10 +1,12 @@
 package lemon.halite3.strategy.generator;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Set;
@@ -17,6 +19,7 @@ import lemon.halite3.strategy.greedy.Quad;
 import lemon.halite3.util.Benchmark;
 import lemon.halite3.util.DebugLog;
 import lemon.halite3.util.Direction;
+import lemon.halite3.util.Dropoff;
 import lemon.halite3.util.GameConstants;
 import lemon.halite3.util.GameMap;
 import lemon.halite3.util.Networking;
@@ -34,93 +37,116 @@ public class GeneratorStrategy implements Strategy {
 	}
 	@Override
 	public void run(GameMap gameMap) {
+		// shipPriority can be optimized using a balanced tree to have O(log(n)) reordering rather than O(n)
+		List<Integer> shipPriorities = new ArrayList<Integer>(); // In order of the last time they dropped off halite
 		Set<Integer> returningShips = new HashSet<Integer>();
 		while (true) {
 			gameMap.update();
-			DebugLog.log("New Turn: " + gameMap.getCurrentTurn() + " - numShips=" + gameMap.getMyPlayer().getShips().size() + 
-					" ***********************************************");
-			try (Benchmark benchmark = new Benchmark("DP: %ss")) {
-				Heuristics.init(gameMap, DP.execute(gameMap));
-			}
-			MoveQueue moveQueue = new MoveQueue(gameMap);
-			Map<Vector, Integer> mineMap = new HashMap<Vector, Integer>();
-			try (Benchmark b3 = new Benchmark("Executed Ships: %ss")) {
-				for (Ship ship : gameMap.getMyPlayer().getShips().values()) {
-					DebugLog.log("Executing Ship: " + ship.toString());
-					// Check if the ship's only option is Direction.STILL
-					if (ship.getHalite() < gameMap.getHalite(ship.getLocation()) / GameConstants.MOVE_COST_RATIO) {
-						DebugLog.log("\tNo Option");
-						moveQueue.move(ship, Direction.STILL);
-						continue;
+			try (Benchmark benchmark = new Benchmark("Benchmark: %ss")) {
+				DebugLog.log("New Turn: " + gameMap.getCurrentTurn() + " - numShips=" + gameMap.getMyPlayer().getShips().size() + 
+						" ***********************************************");
+				try (Benchmark b = new Benchmark("DP: %ss")) {
+					Heuristics.init(gameMap, DP.execute(gameMap));
+				}
+				MoveQueue moveQueue = new MoveQueue(gameMap);
+				Map<Vector, Integer> mineMap = new HashMap<Vector, Integer>();
+				// updates shipPriority
+				DebugLog.log("Updating Ship Priorities...");
+				try (Benchmark b2 = new Benchmark("Updated Ship Priorities: %ss")) {
+					Ship shipyardCandidate = gameMap.getShip(gameMap.getMyPlayer().getShipyardLocation());
+					if (shipyardCandidate != null) {
+						shipPriorities.remove((Object) shipyardCandidate.getShipId());
+						shipPriorities.add(shipyardCandidate.getShipId());
 					}
-					int haliteNeeded = GameConstants.MAX_HALITE - ship.getHalite() - 50;
-					DebugLog.log("\tHalite Needed: " + haliteNeeded);
-					if (haliteNeeded <= 0) {
-						returningShips.add(ship.getShipId());
+					for (Dropoff dropoff : gameMap.getMyPlayer().getDropoffs().values()) {
+						Ship candidate = gameMap.getShip(dropoff.getLocation());
+						if (candidate != null) {
+							shipPriorities.remove((Object) candidate.getShipId());
+							shipPriorities.add(candidate.getShipId());
+						}
 					}
-					if (returningShips.contains(ship.getShipId())) {
-						if (ship.getHalite() <= GameConstants.MAX_HALITE / 5) {
-							returningShips.remove(ship.getShipId());
-						} else {
-							DebugLog.log("\tReturning to shipyard");
-							moveQueue.move(ship, new Navigation(gameMap).navigate(ship.getLocation(), gameMap.getMyPlayer().getShipyardLocation())); // TODO dropoffs
+					// Remove dead ships
+					shipPriorities.removeIf(shipId -> !gameMap.getMyPlayer().getShips().containsKey(shipId));
+				}
+				try (Benchmark b3 = new Benchmark("Executed Ships: %ss")) {
+					for (int shipId : shipPriorities) {
+						Ship ship = gameMap.getMyPlayer().getShips().get(shipId);
+						DebugLog.log("Executing Ship: " + ship.toString());
+						// Check if the ship's only option is Direction.STILL
+						if (ship.getHalite() < gameMap.getHalite(ship.getLocation()) / GameConstants.MOVE_COST_RATIO) {
+							DebugLog.log("\tNo Option");
+							moveQueue.move(ship, Direction.STILL);
 							continue;
 						}
-					}
-					// BFS search for mine target
-					Deque<Vector> queue = new ArrayDeque<Vector>();
-					Set<Vector> visited = new HashSet<Vector>();
-					HeuristicsPlan bestPlan = null;
-					int bestPlanScore = Integer.MAX_VALUE;
-					Quad bestQuad = null;
-					queue.add(ship.getLocation());
-					visited.add(ship.getLocation());
-					while (!queue.isEmpty()) {
-						Vector vector = queue.poll();
-						// TODO: break when there's no point of looking for more (bestPlanScore < vector.getManhattanDistance(vector, gameMap))
-						for (int i = 0; i < 8; ++i) {
-							Quad quad = getQuad(vector, i);
-							if (getHaliteCount(quad, mineMap) > haliteNeeded * 4) { // Arbitrary threshold greater than haliteNeeded
-								Set<Vector> vectors = getVectors(mineMap, quad, haliteNeeded);
-								HeuristicsPlan plan = getPlan(mineMap, vectors, GameConstants.MAX_HALITE - 50, ship);
-								int score = plan.getTotalTurns();
-								//benchmark.peek("\t\tFound MinePlan: " + plan + " - " + turns + " - " + bestPlanTurns + " - %s");
-								if (score < bestPlanScore) {
-									bestPlan = plan;
-									bestPlanScore = score;
-									bestQuad = quad;
+						int haliteNeeded = GameConstants.MAX_HALITE - ship.getHalite() - 50;
+						DebugLog.log("\tHalite Needed: " + haliteNeeded);
+						if (haliteNeeded <= 0) {
+							returningShips.add(ship.getShipId());
+						}
+						if (returningShips.contains(ship.getShipId())) {
+							if (ship.getHalite() <= GameConstants.MAX_HALITE / 5) {
+								returningShips.remove(ship.getShipId());
+							} else {
+								DebugLog.log("\tReturning to shipyard");
+								moveQueue.move(ship, new Navigation(gameMap).navigate(ship.getLocation(), gameMap.getMyPlayer().getShipyardLocation())); // TODO dropoffs
+								continue;
+							}
+						}
+						// BFS search for mine target
+						Deque<Vector> queue = new ArrayDeque<Vector>();
+						Set<Vector> visited = new HashSet<Vector>();
+						HeuristicsPlan bestPlan = null;
+						int bestPlanScore = Integer.MAX_VALUE;
+						Quad bestQuad = null;
+						queue.add(ship.getLocation());
+						visited.add(ship.getLocation());
+						while (!queue.isEmpty()) {
+							Vector vector = queue.poll();
+							// TODO: break when there's no point of looking for more (bestPlanScore < vector.getManhattanDistance(vector, gameMap))
+							for (int i = 0; i < 8; ++i) {
+								Quad quad = getQuad(vector, i);
+								if (getHaliteCount(quad, mineMap) > haliteNeeded * 4) { // Arbitrary threshold greater than haliteNeeded
+									Set<Vector> vectors = getVectors(mineMap, quad, haliteNeeded);
+									HeuristicsPlan plan = getPlan(mineMap, vectors, GameConstants.MAX_HALITE - 50, ship);
+									int score = plan.getTotalTurns();
+									//benchmark.peek("\t\tFound MinePlan: " + plan + " - " + turns + " - " + bestPlanTurns + " - %s");
+									if (score < bestPlanScore) {
+										bestPlan = plan;
+										bestPlanScore = score;
+										bestQuad = quad;
+									}
+									break;
 								}
-								break;
+							}
+							for (Direction direction : Direction.CARDINAL_DIRECTIONS) {
+								Vector candidate = vector.add(direction, gameMap);
+								if (!visited.contains(candidate)) {
+									queue.add(candidate);
+									visited.add(candidate);
+								}
 							}
 						}
-						for (Direction direction : Direction.CARDINAL_DIRECTIONS) {
-							Vector candidate = vector.add(direction, gameMap);
-							if (!visited.contains(candidate)) {
-								queue.add(candidate);
-								visited.add(candidate);
+						if (bestPlan != null) {
+							// Apply bestPlan's mineMap
+							for (Vector vector : bestPlan.getMineMap().keySet()) {
+								mineMap.put(vector, mineMap.getOrDefault(vector, 0) + bestPlan.getMineMap().get(vector));
 							}
+							handleMicro(moveQueue, ship, bestPlan, bestQuad);
 						}
 					}
-					if (bestPlan != null) {
-						// Apply bestPlan's mineMap
-						for (Vector vector : bestPlan.getMineMap().keySet()) {
-							mineMap.put(vector, mineMap.getOrDefault(vector, 0) + bestPlan.getMineMap().get(vector));
-						}
-						handleMicro(moveQueue, ship, bestPlan, bestQuad);
+				}
+				moveQueue.resolveCollisions(shipPriorities);
+				// Try to spawn a ship
+				if (moveQueue.isSafe(gameMap.getMyPlayer().getShipyardLocation())) {
+					if (gameMap.getMyPlayer().getShips().size() == 0 && 
+							gameMap.getMyPlayer().getHalite() >= GameConstants.SHIP_COST && // TODO - consider cost of building dropoffs in the same turn
+							gameMap.getCurrentTurn() + 150 < GameConstants.MAX_TURNS) {
+						Networking.spawnShip();
 					}
 				}
+				moveQueue.send();
+				Networking.endTurn();
 			}
-			moveQueue.resolveCollisions();
-			// Try to spawn a ship
-			if (moveQueue.isSafe(gameMap.getMyPlayer().getShipyardLocation())) {
-				if (gameMap.getMyPlayer().getShips().size() == 0 && 
-						gameMap.getMyPlayer().getHalite() >= GameConstants.SHIP_COST && // TODO - consider cost of building dropoffs in the same turn
-						gameMap.getCurrentTurn() + 150 < GameConstants.MAX_TURNS) {
-					Networking.spawnShip();
-				}
-			}
-			moveQueue.send();
-			Networking.endTurn();
 		}
 	}
 	public void handleMicro(MoveQueue moveQueue, Ship ship, HeuristicsPlan plan, Quad quad) {
