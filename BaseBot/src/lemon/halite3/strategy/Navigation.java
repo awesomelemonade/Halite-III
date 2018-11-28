90c3d125d7f3ce93e109738356c09dacfcabda1c
awesomelemonade <awesomelemonade@gmail.com>
2018-11-28 10:00:59 -0500

    Rewrote Navigation to use new DP

--- BaseBot/src/lemon/halite3/strategy/Navigation.java
+++ BaseBot/src/lemon/halite3/strategy/Navigation.java
@@ -1,60 +1,24 @@
 package lemon.halite3.strategy;
 
+import lemon.halite3.strategy.generator.DP;
 import lemon.halite3.util.Direction;
-import lemon.halite3.util.GameConstants;
 import lemon.halite3.util.GameMap;
 import lemon.halite3.util.Vector;
 
 public class Navigation {
-	private GameMap gameMap;
-	private int[][] dp;
-	public Navigation(GameMap gameMap) {
-		this.gameMap = gameMap;
-		this.dp = new int[gameMap.getWidth()][gameMap.getHeight()];
+	private static GameMap gameMap;
+	public static void init(GameMap gameMap) {
+		Navigation.gameMap = gameMap;
 	}
-	public int getCost(Vector start, Vector end) {
-		if (start.equals(end)) {
-			return 0;
-		}
-		Direction a = getDirection(start.getX(), end.getX(), gameMap.getWidth(), Direction.WEST, Direction.EAST);
-		Direction b = getDirection(start.getY(), end.getY(), gameMap.getHeight(), Direction.NORTH, Direction.SOUTH);
-		for (int i = 0; i < dp.length; ++i) {
-			for (int j = 0; j < dp[0].length; ++j) {
-				dp[i][j] = Integer.MAX_VALUE;
-			}
-		}
-		dp[end.getX()][end.getY()] = 0;
-		return dp(start, end, dp, a, b);
-	}
-	public Direction navigate(Vector start, Vector end) {
+	public static Direction navigate(Vector start, Vector end) {
 		if (start.equals(end)) {
 			return Direction.STILL;
 		}
 		Direction a = getDirection(start.getX(), end.getX(), gameMap.getWidth(), Direction.WEST, Direction.EAST);
 		Direction b = getDirection(start.getY(), end.getY(), gameMap.getHeight(), Direction.NORTH, Direction.SOUTH);
-		for (int i = 0; i < dp.length; ++i) {
-			for (int j = 0; j < dp[0].length; ++j) {
-				dp[i][j] = Integer.MAX_VALUE;
-			}
-		}
-		dp[end.getX()][end.getY()] = 0;
-		dp(start, end, dp, a, b);
-		Vector aVector = start.add(a, gameMap);
-		Vector bVector = start.add(b, gameMap);
-		return dp[aVector.getX()][aVector.getY()] <
-				dp[bVector.getX()][bVector.getY()] ? a : b;
-	}
-	public int dp(Vector vector, Vector end, int[][] dp, Direction a, Direction b) {
-		if (dp[vector.getX()][vector.getY()] != Integer.MAX_VALUE) {
-			return dp[vector.getX()][vector.getY()];
-		}
-		Vector aVector = vector.add(a, gameMap);
-		Vector bVector = vector.add(b, gameMap);
-		int aDP = vector.getX() == end.getX() ? Integer.MAX_VALUE : dp(aVector, end, dp, a, b);
-		int bDP = vector.getY() == end.getY() ? Integer.MAX_VALUE : dp(bVector, end, dp, a, b);
-		int result = Math.min(aDP, bDP) + gameMap.getHalite(vector) / GameConstants.MOVE_COST_RATIO;
-		dp[vector.getX()][vector.getY()] = result;
-		return result;
+		int aDP = start.getX() == end.getX() ? Integer.MAX_VALUE : DP.getCost(start.add(a, gameMap), end);
+		int bDP = start.getY() == end.getY() ? Integer.MAX_VALUE : DP.getCost(start.add(b, gameMap), end);
+		return aDP < bDP ? a : b;
 	}
 	public static Direction getDirection(int start, int end, int mod, Direction neg, Direction pos) {
 		return end > start ? (end - start < mod - end + start ? pos : neg) : (start - end < mod - start + end ? neg : pos);
