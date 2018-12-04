2119b1308064b789babdfdff458222026cff9eff
awesomelemonade <awesomelemonade@gmail.com>
2018-12-04 10:19:19 -0500

    Initial trueskill for botarena

--- BotArena/botarena.py
+++ BotArena/botarena.py
@@ -6,4 +6,13 @@ import subprocess
 def runMatch(playerA, playerB):
     return json.loads(subprocess.run(['halite', '--results-as-json', playerA, playerB], stdout=subprocess.PIPE).stdout.decode('utf-8'))
 
-print("test")
+from trueskill import Rating, quality_1vs1, rate_1vs1
+
+a = trueskill.Rating(mu=25.000, sigma=8.333)
+b = trueskill.Rating(mu=25.000, sigma=8.333)
+
+a, b = rate_1vs1(a, b)
+
+# Worker Tasks
+# git pull, lock, compile, unlock
+# run, lock, get results, update mu/sigma, unlock
