154b70f4dd13fa4f381540fc2fc24d1b2cc92ae3
awesomelemonade <awesomelemonade@gmail.com>
2018-12-04 20:54:31 -0500

    Added priority of movement in Navigation

--- BaseBot/src/lemon/halite3/strategy/Navigation.java
+++ BaseBot/src/lemon/halite3/strategy/Navigation.java
@@ -10,15 +10,15 @@ public class Navigation {
 	public static void init(GameMap gameMap) {
 		Navigation.gameMap = gameMap;
 	}
-	public static Direction navigate(Vector start, Vector end) {
+	public static Direction[] navigate(Vector start, Vector end) {
 		if (start.equals(end)) {
-			return Direction.STILL;
+			return new Direction[] {Direction.STILL};
 		}
 		Direction a = getDirection(start.getX(), end.getX(), gameMap.getWidth(), Direction.WEST, Direction.EAST);
 		Direction b = getDirection(start.getY(), end.getY(), gameMap.getHeight(), Direction.NORTH, Direction.SOUTH);
 		int aDP = start.getX() == end.getX() ? Integer.MAX_VALUE : DP.getCost(start.add(a, gameMap), end);
 		int bDP = start.getY() == end.getY() ? Integer.MAX_VALUE : DP.getCost(start.add(b, gameMap), end);
-		return aDP < bDP ? a : b;
+		return aDP < bDP ? new Direction[] {a, b} : new Direction[] {b, a};
 	}
 	public static Direction getDirection(int start, int end, int mod, Direction neg, Direction pos) {
 		return end > start ? (end - start < mod - end + start ? pos : neg) : (start - end < mod - start + end ? neg : pos);
