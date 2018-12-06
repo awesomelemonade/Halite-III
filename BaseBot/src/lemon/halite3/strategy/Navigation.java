5cb15f569e3cae5ed7d04110f49c710700c1dd63
awesomelemonade <awesomelemonade@gmail.com>
2018-12-05 21:11:27 -0500

    Navigation speed up

--- BaseBot/src/lemon/halite3/strategy/Navigation.java
+++ BaseBot/src/lemon/halite3/strategy/Navigation.java
@@ -16,9 +16,13 @@ public class Navigation {
 		}
 		Direction a = getDirection(start.getX(), end.getX(), gameMap.getWidth(), Direction.WEST, Direction.EAST);
 		Direction b = getDirection(start.getY(), end.getY(), gameMap.getHeight(), Direction.NORTH, Direction.SOUTH);
-		int aDP = start.getX() == end.getX() ? Integer.MAX_VALUE : DP.getCost(start.add(a, gameMap), end);
-		int bDP = start.getY() == end.getY() ? Integer.MAX_VALUE : DP.getCost(start.add(b, gameMap), end);
-		return aDP < bDP ? new Direction[] {a, b} : new Direction[] {b, a};
+		if (start.getX() == end.getX()) {
+			return new Direction[] {b};
+		}
+		if (start.getY() == end.getY()) {
+			return new Direction[] {a};
+		}
+		return DP.getCost(start.add(a, gameMap), end) < DP.getCost(start.add(b, gameMap), end) ? new Direction[] {a, b} : new Direction[] {b, a};
 	}
 	public static Direction getDirection(int start, int end, int mod, Direction neg, Direction pos) {
 		return end > start ? (end - start < mod - end + start ? pos : neg) : (start - end < mod - start + end ? neg : pos);
